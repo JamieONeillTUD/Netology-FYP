fe9bc169acaea2bc39b2ef1643bb17cbb0c6f3cc
JamieONeillTUD <C22320301@mytudublin.ie>
2025-11-10 12:25:33 +0000

    db connection

--- Netology/backend/config.py
+++ Netology/backend/config.py
@@ -1,7 +1,7 @@
 
 # PostgreSQL connection details (AWS RDS or Docker)
 DB_CONFIG = {
-    'host': 'db',          # or your-rds-endpoint.rds.amazonaws.com
+    'host': 'netology-db.c58saiqicrvi.eu-west-1.rds.amazonaws.com',          # or your-rds-endpoint.rds.amazonaws.com
     'port': 5432,
     'database': 'netology',
     'user': 'netology',
