fd5c2462ee595f040c403c015f8c67f115c189b0
JamieONeillTUD <C22320301@mytudublin.ie>
2025-11-30 14:26:58 +0000

    Update to Database connection

--- Netology/backend/db.py
+++ Netology/backend/db.py
@@ -4,23 +4,16 @@ Student Name: Jamie O’Neill
 Course Code: TU857/4
 Date: 10/11/2025
 
-Python (Flask) – Netology Learning Platform
+Python (Flask) 
 -------------------------------------------
 db.py – Database connection module.
+
 Creates and returns a connection to the AWS PostgreSQL database.
 Used by all backend routes (auth_routes, course_routes, xp_system).
 """
 
 import psycopg
-
-# =====================================================
-# DATABASE CONNECTION
-# =====================================================
 def get_db_connection():
-    """
-    Connect to AWS RDS PostgreSQL database and return the connection.
-    Each route will open and close this connection as needed.
-    """
     return psycopg.connect(
         host="netology-db.c58saiqicrvi.eu-west-1.rds.amazonaws.com",
         dbname="postgres",
